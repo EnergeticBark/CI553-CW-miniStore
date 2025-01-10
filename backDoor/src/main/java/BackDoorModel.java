c23e721d583baf1081c5ba6331110ef4b50b51e9
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-01-10 17:02:45 +0000

    Connect new BackDoorView to the BackDoorModel.

--- backDoor/src/main/java/BackDoorModel.java
+++ backDoor/src/main/java/BackDoorModel.java
@@ -1,24 +1,34 @@
-import catalogue.Basket;
-import catalogue.BetterBasket;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import products.Product;
 import products.exceptions.ProductDoesNotExistException;
 import javafx.beans.property.SimpleStringProperty;
 import products.ProductDAO;
 import middle.MiddleFactory;
 import dao.DAOException;
-import products.usecases.GetProductByNumber;
 import products.usecases.RestockProduct;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 /**
  * Implements the Model of the back door client
  */
 public class BackDoorModel {
-    private Basket theBasket = null; // Bought items
+    private Product product = null;
+
+    final ObjectProperty<ObservableList<Product>> stockList = new SimpleObjectProperty<>();
+
+    final SimpleStringProperty productDescription = new SimpleStringProperty();
+    final SimpleStringProperty productPrice = new SimpleStringProperty();
+    final SimpleStringProperty productQuantity = new SimpleStringProperty();
+    final SimpleStringProperty productNumber = new SimpleStringProperty();
 
     private ProductDAO stockDAO = null;
 
     final SimpleStringProperty action = new SimpleStringProperty();
-    final SimpleStringProperty output = new SimpleStringProperty();
 
     private static final System.Logger LOGGER = System.getLogger(BackDoorModel.class.getName());
 
@@ -29,90 +39,55 @@ public class BackDoorModel {
     public BackDoorModel(MiddleFactory mf) {
         try {
             stockDAO = mf.makeStockDAO(); // Database access
+            stockList.setValue(FXCollections.observableList(stockDAO.getAll()));
         } catch (Exception e) {
             LOGGER.log(System.Logger.Level.ERROR, e);
             System.exit(-1);
         }
-
-        theBasket = makeBasket(); // Initial Basket
     }
 
     // Tell the BackDoorView that the model has changed, so it needs to redraw.
     private void fireAction(String actionMessage) {
         this.action.setValue(actionMessage);
-        this.output.setValue(getBasket().getDetails());
+
+        productDescription.setValue(product.getDescription());
+        productPrice.setValue(
+                NumberFormat.getCurrencyInstance(Locale.UK).format(
+                        product.getPrice()
+                )
+        );
+        productNumber.setValue(String.format("Product#: %04d", product.getProductNumber()));
+        productQuantity.setValue(String.format("Quantity In Stock: (%d)", product.getQuantity()));
     }
 
-    /**
-     * Get the Basket of products
-     * @return basket
-     */
-    public Basket getBasket() {
-        return theBasket;
-    }
-
-    /**
-     * Query
-     * @param productNumber The product number of the item
-     */
-    public void query(String productNumber) {
-        String trimmedProductNumber = productNumber.trim();
-        try {
-            int parsedProductNumber = Integer.parseUnsignedInt(trimmedProductNumber);
-            Product product = new GetProductByNumber(stockDAO).run(parsedProductNumber);
-            fireAction(product.showDetails());
-        } catch (ProductDoesNotExistException | NumberFormatException _) {
-            fireAction("Unknown product number " + trimmedProductNumber);
-        } catch (DAOException e) {
-            fireAction(e.getMessage());
-        }
+    public void selectProduct(Product product) {
+        this.product = product;
+        fireAction("");
     }
 
     /**
      * Re stock
-     * @param productNumber The product number of the item
      * @param quantity How many to be added
      */
-    public void restock(String productNumber, String quantity) {
-        theBasket = makeBasket();
-        String trimmedProductNumber = productNumber.trim();
-        String trimmedQuantity = quantity.trim();
-
-        int parsedQuantity;
-        try {
-            parsedQuantity = Integer.parseUnsignedInt(trimmedQuantity);
-        } catch (NumberFormatException e) {
-            fireAction("Invalid quantity");
-            return;
+    public void restock(int quantity) {
+        if (product == null) {
+            // TODO show this in the UI instead.
+            throw new RuntimeException("No product selected.");
         }
 
         try {
-            int parsedProductNumber = Integer.parseUnsignedInt(trimmedProductNumber);
-            Product product = new RestockProduct(stockDAO).run(parsedProductNumber, parsedQuantity);
-
-            theBasket.add(product);
+            product = new RestockProduct(stockDAO).run(product.getProductNumber(), quantity);
             fireAction("");
+            try {
+                // Refresh the stock list.
+                stockList.setValue(FXCollections.observableList(stockDAO.getAll()));
+            } catch (DAOException e) {
+                throw new RuntimeException(e);
+            }
         } catch (ProductDoesNotExistException | NumberFormatException _) {
-            fireAction("Unknown product number " + trimmedProductNumber);
+            fireAction("Unknown product number " + product.getProductNumber());
         } catch (DAOException e) {
             fireAction(e.getMessage());
         }
     }
-
-    /**
-     * Clear the product()
-     */
-    public void clear() {
-        theBasket.clear(); // Clear s. list
-        fireAction("Enter Product Number"); // Set display
-    }
-
-    /**
-     * return an instance of a Basket
-     * @return a new instance of a Basket
-     */
-    protected Basket makeBasket() {
-        return new BetterBasket();
-    }
 }
-
--- backDoor/src/main/java/BackDoorView.java
+++ backDoor/src/main/java/BackDoorView.java
@@ -1,18 +1,12 @@
 import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import products.Product;
-
-import java.util.List;
 
 /**
  * Implements the Customer view.
@@ -21,13 +15,6 @@ public class BackDoorView {
     // Width and height of the window in pixels.
     private static final int WIDTH = 500;
     private static final int HEIGHT = 340;
-
-    // Width and height of the button in pixels.
-    private static final int BUTTON_WIDTH = 80;
-    private static final int BUTTON_HEIGHT = 35;
-
-    private final TextField inputField = new TextField();
-    private final TextField inputField2 = new TextField();
 
     private BackDoorController controller = null;
 
@@ -41,84 +28,7 @@ public class BackDoorView {
         stage.setX(x);
         stage.setY(y);
 
-        TableColumn<Product, String> productNumberColumn = new TableColumn<>("Product Number");
-        productNumberColumn.setCellValueFactory(product -> {
-            int productNumber = product.getValue().getProductNumber();
-            String formattedProductNumber = String.format("%04d", productNumber);
-            return new ReadOnlyObjectWrapper<>(formattedProductNumber);
-        });
-        productNumberColumn.setResizable(false);
-        productNumberColumn.setPrefWidth(110);
-
-        TableColumn<Product, Integer> productQuantityColumn = new TableColumn<>("Qty. In Stock");
-        productQuantityColumn.setCellValueFactory(product -> new ReadOnlyObjectWrapper<>(product.getValue().getQuantity()));
-        productQuantityColumn.setResizable(false);
-        productQuantityColumn.setPrefWidth(100);
-
-        ObservableList<Product> fakeList = FXCollections.observableList(
-                List.of(
-                        new Product(1, "", 0, 90),
-                        new Product(2, "", 0, 20),
-                        new Product(3, "", 0, 33),
-                        new Product(4, "", 0, 10),
-                        new Product(5, "", 0, 17),
-                        new Product(6, "", 0, 15),
-                        new Product(7, "", 0, 1),
-                        new Product(8, "", 0, 32),
-                        new Product(9, "", 0, 63)
-                )
-        );
-
-        TableView<Product> productTable = new TableView<>();
-        productTable.setItems(fakeList);
-        productTable.getColumns().add(productNumberColumn);
-        productTable.getColumns().add(productQuantityColumn);
-        VBox.setVgrow(productTable, Priority.ALWAYS);
-
-        productTable.setPrefWidth(226);
-
-        Label productDescription = new Label("Panasonic Blu-ray");
-        productDescription.setId("product-description");
-
-        Label productPrice = new Label("$99.00");
-        productPrice.setId("product-price");
-
-        Label productNumber = new Label("Product#: 0009");
-        productDescription.setId("product-description");
-
-        VBox productPane = new VBox(productDescription, productPrice, productNumber);
-
-        TitledPane selectedPane = new TitledPane("Selected Product", productPane);
-        selectedPane.collapsibleProperty().setValue(false);
-        VBox.setVgrow(selectedPane, Priority.ALWAYS);
-
-        Label productQuantity = new Label("Quantity In Stock: (63)");
-        productQuantity.setId("product-quantity");
-
-        Label plusLabel = new Label("+");
-        plusLabel.setId("plus-label");
-
-        Spinner<Integer> spinner = new Spinner<>(0, 99999 , 1);
-        spinner.setEditable(true);
-        spinner.setId("spinner");
-
-        Pane spacer = new Pane();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-
-        Button restockButton = new Button("Restock");
-
-        HBox inputPane = new HBox(plusLabel, spinner, spacer, restockButton);
-
-        VBox quantityPane = new VBox(productQuantity, inputPane);
-        quantityPane.setSpacing(10);
-
-        TitledPane stockPane = new TitledPane("Modify Stock", quantityPane);
-        stockPane.collapsibleProperty().setValue(false);
-
-        VBox rightPane = new VBox(selectedPane, stockPane);
-        HBox.setHgrow(rightPane, Priority.ALWAYS);
-
-        HBox hBox = new HBox(productTable, new Separator(), rightPane);
+        HBox hBox = new HBox(makeProductTable(model), new Separator(), makeRightPane(model));
 
         Scene scene = new Scene(hBox);
         stage.setWidth(WIDTH);
@@ -131,44 +41,78 @@ public class BackDoorView {
         stage.show();
     }
 
-    private VBox makeLeftPane() {
-        Button queryButton = new Button("Query");
-        queryButton.setOnAction(_ -> controller.query(inputField.getText()));
-        queryButton.setMinSize(Control.USE_PREF_SIZE, Control.USE_PREF_SIZE);
-        queryButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+    private TableView<Product> makeProductTable(BackDoorModel model) {
+        TableColumn<Product, String> productNumberColumn = new TableColumn<>("Product Number");
+        productNumberColumn.setCellValueFactory(product -> {
+            int productNumber = product.getValue().getProductNumber();
+            String formattedProductNumber = String.format("%04d", productNumber);
+            return new ReadOnlyObjectWrapper<>(formattedProductNumber);
+        });
+        productNumberColumn.setResizable(false);
+        productNumberColumn.setId("number-column");
 
-        Button restockButton = new Button("Restock");
-        restockButton.setOnAction(_ -> controller.restock(inputField.getText(), inputField2.getText()));
-        restockButton.setMinSize(Control.USE_PREF_SIZE, Control.USE_PREF_SIZE);
-        restockButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        TableColumn<Product, Integer> productQuantityColumn = new TableColumn<>("Qty. In Stock");
+        productQuantityColumn.setCellValueFactory(product -> new ReadOnlyObjectWrapper<>(product.getValue().getQuantity()));
+        productQuantityColumn.setResizable(false);
+        productQuantityColumn.setId("quantity-column");
 
-        Button clearButton = new Button("Clear");
-        clearButton.setOnAction(_ -> controller.clear());
-        clearButton.setMinSize(Control.USE_PREF_SIZE, Control.USE_PREF_SIZE);
-        clearButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-
-        VBox vBox = new VBox(queryButton, restockButton, clearButton);
-        vBox.setSpacing(16);
-        vBox.setPadding(new Insets(25, 0, 25, 0));
-        return vBox;
+        TableView<Product> productTable = new TableView<>();
+        productTable.itemsProperty().bind(model.stockList);
+        productTable.getColumns().add(productNumberColumn);
+        productTable.getColumns().add(productQuantityColumn);
+        productTable.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((_, _, product) -> controller.selectProduct(product));
+        VBox.setVgrow(productTable, Priority.ALWAYS);
+        productTable.setId("product-table");
+        return productTable;
     }
 
     private VBox makeRightPane(BackDoorModel model) {
-        Label pageTitle = new Label("Staff check and manage stock");
+        Label productDescription = new Label();
+        productDescription.textProperty().bind(model.productDescription);
+        productDescription.setId("product-description");
 
-        Label actionLabel = new Label();
-        actionLabel.textProperty().bind(model.action);
+        Label productPrice = new Label();
+        productPrice.textProperty().bind(model.productPrice);
+        productPrice.setId("product-price");
 
-        TextArea outputText = new TextArea();
-        outputText.textProperty().bind(model.output);
-        outputText.setFont(Font.font("Monospaced", 12));
+        Label productNumber = new Label();
+        productNumber.textProperty().bind(model.productNumber);
+        productDescription.setId("product-description");
 
-        HBox hBox = new HBox(inputField, inputField2);
-        hBox.setSpacing(30);
+        VBox productPane = new VBox(productDescription, productPrice, productNumber);
 
-        VBox vBox = new VBox(pageTitle, actionLabel, hBox, outputText);
-        vBox.setSpacing(10);
-        return vBox;
+        TitledPane selectedPane = new TitledPane("Selected Product", productPane);
+        VBox.setVgrow(selectedPane, Priority.ALWAYS);
+
+        Label productQuantity = new Label();
+        productQuantity.textProperty().bind(model.productQuantity);
+        productQuantity.setId("product-quantity");
+
+        Label plusLabel = new Label("+");
+        plusLabel.setId("plus-label");
+
+        Spinner<Integer> spinner = new Spinner<>(0, 99999 , 0);
+        spinner.setEditable(true);
+        spinner.setId("spinner");
+
+        Pane spacer = new Pane();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        Button restockButton = new Button("Restock");
+        restockButton.setOnAction(_ -> controller.restock(spinner.getValue()));
+
+        HBox inputPane = new HBox(plusLabel, spinner, spacer, restockButton);
+
+        VBox quantityPane = new VBox(productQuantity, inputPane);
+        quantityPane.setId("quantity-pane");
+
+        TitledPane stockPane = new TitledPane("Modify Stock", quantityPane);
+
+        VBox rightPane = new VBox(selectedPane, stockPane);
+        HBox.setHgrow(rightPane, Priority.ALWAYS);
+        return rightPane;
     }
 
     public void setController(BackDoorController controller) {
